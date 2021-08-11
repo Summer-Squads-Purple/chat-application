fe0174f1c1a73766dea04c33bc726d62b1f8116c
Alphairus <cmcauliffe006@gmail.com>
2021-08-11 12:50:43 -0400

    chat functional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from "react-chat-engine";
 
-function App() {
+import "./App.css";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="7c477590-16b5-4b11-8818-066d92e5c0a3"
+      userName="Alpharius"
+      userSecret="123123"
+    />
   );
-}
+};
 
 export default App;
